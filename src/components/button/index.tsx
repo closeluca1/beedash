5dcb60e9150f67ab6428bc6862bbd78302f3a9d2
closeluca1 <contatopatricklucas@gmail.com>
2022-05-30 17:16:49 -0300

    fixed files

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,13 +1,13 @@
 interface ButtonProps {
   onClick: any;
   title: string;
-  disabled: boolean;
+  disabled: any;
 }
 
 export function Button({ onClick, title, disabled }: ButtonProps) {
   return (
     <button
-      className='bg-indigo-600 hover:bg-indigo-900 transition-all duration-200 text-zinc-50 rounded-md w-full md:max-w-sm h-10 mt-5 text-lg font-normal'
+      className='bg-indigo-600 hover:bg-indigo-900 transition-all duration-200 text-zinc-50 rounded-md w-full md:max-w-sm h-10 mt-6 text-lg font-normal disabled:opacity-75 disabled:bg-indigo-900'
       onClick={onClick}
       disabled={disabled}
     >
