39379fb4c255fc4c6febd245bccb5c72084e195c
closeluca1 <contatopatricklucas@gmail.com>
2022-05-31 15:38:51 -0300

    creat footer component

---
+++ src/views/footer/index.tsx
@@ -0,0 +1,7 @@
+export function Footer () {
+  return (
+    <div className='w-full flex items-center justify-center py-5 mt-[2vh]'>
+      <h6 className='text-sm '>2022 - &copy; Criado por <a className='text-indigo-500 hover:text-indigo-900 transition-all duration-200' href='https://github.com/closeluca1' target='_blank'>closeluca1</a></h6>
+    </div>
+  )
+}
