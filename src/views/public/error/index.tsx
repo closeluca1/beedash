9b6c22866b06f1c52b0333b1826762a7ae50abcf
closeluca1 <contatopatricklucas@gmail.com>
2022-05-31 15:39:28 -0300

    styled page error404

--- src/views/public/error/index.tsx
+++ src/views/public/error/index.tsx
@@ -1,5 +1,26 @@
-export function Error404 () {
+import { useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+import 'dotenv'
+
+export function Error404() {
+
+  const auth = getAuth();
+
+  const [verifyUser, setVerifyUser] = useState<string | null>()
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setVerifyUser(import.meta.env.VITE_USER_DASHBOARD);
+    } else {
+      setVerifyUser(import.meta.env.VITE_USER_LOGIN);
+    }
+  });
+
   return (
-    <>erro 404</>
+    <div className='w-full min-h-[80vh] flex flex-col items-center justify-center'>
+      <h5 className='text-lg'>Algo errado não esta certo!</h5>
+      <a href={`${verifyUser}`}><h6 className='text-sm text-indigo-500 hover:text-indigo-900 transition-all duration-200'>voltar para página inicial</h6></a>
+    </div>
   )
 } 
